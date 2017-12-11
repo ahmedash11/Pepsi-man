610eafc8a23099314ce7c594099188f84ae5afd0
Mohammad Elhinamy <hinamy@Mohammads-MacBook-Pro.local>
2017-12-11 19:12:19 +0200

    Initial Commit

---
+++ graPro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  graPro
+//
+//  Created by Mohammad Elhinamy on 12/11/17.
+//  Copyright © 2017 Mohammad Elhinamy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
